a0296309febfdcfc85cf440e396f438f4a3d7f7e
Robson Barbosa Souza <robson.souza@olgari.com.br>
2022-04-19 09:24:16 -0300

    chore: fix install dependecies

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -3,3 +3,4 @@ echo "Instalando NodeJS"
 curl -fsSL http://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install --global yarn
+cd /persistencia/app && yarn
